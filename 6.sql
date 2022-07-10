3cf2df8d753a42b03744d237fa7d0ed57dc98d6f
Stefannia Thaina <78321194+thainabrito@users.noreply.github.com>
2022-07-10 03:28:18 -0300

    Delete 6.sql

--- 6.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(*) FROM sakila.rental;
-SELECT * FROM sakila.rental;
-SELECT * FROM sakila.rental LIMIT 10;
-SELECT * FROM sakila.rental LIMIT 10 OFFSET 3;
-SELECT * FROM sakila.actor LIMIT 10 OFFSET 4;
